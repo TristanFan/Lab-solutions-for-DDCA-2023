b34f1620b299f3809cb29739c362e7f6d5bfdcf4
TristanFan <tristanfan0626@gmail.com>
2025-09-12 12:02:38 +0800

    Lab8

---
+++ Lab8/snake_patterns.asm
@@ -0,0 +1,34 @@
+### Aanjhan Ranganathan <raanjhan@inf.ethz.ch>
+### For Lab 8 Digital Technik 
+### 2014
+
+###   I/O addresses Reference
+###  compatible to the compact modelling
+###  0x00007FF0   LED output
+
+.data
+pattern: .word 0x00200000,0x00004000,0x00000080,0x00000001,0x00000002,0x00000004,0x00000008,0x00000400,0x00020000,0x01000000,0x02000000,0x04000000
+loopcnt: .word 0x001e8484
+
+.text
+   lw $t3, loopcnt # initialize a  large loopcounter (so that the snake does not crawl SUPERFAST)
+   addi $t5,$0,48       # initialize the length of the display pattern (in bytes)
+   
+restart:   
+   lw    $t1, 0x7ff4($0)    
+   andi $t1, $t1, 0x3
+   addi $t1, $t1, 1
+   addi $t4,$0,0
+
+forward:
+   beq $t5,$t4, restart
+   lw $t0,0($t4)
+   sw  $t0, 0x7ff0($0)
+   
+   addi $t4, $t4, 4 
+   addi $t2, $0, 0 
+
+wait:
+   bge $t2,$t3,forward	
+   add  $t2, $t2, $t1     
+   j wait
